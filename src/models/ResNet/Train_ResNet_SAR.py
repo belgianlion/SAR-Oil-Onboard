de98c8ea984b129c6c69b57e88a8377978fbf352
Mitchell Sylvia <msylvia@umass.edu>
2024-11-14 19:46:40 -0500

    Update with Windows code

--- src/models/ResNet/Train_ResNet_SAR.py
+++ src/models/ResNet/Train_ResNet_SAR.py
@@ -5,6 +5,17 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 import os
 from tqdm import tqdm
+
+print(f'PyTorch version: {torch.__version__}')
+print('*'*10)
+print(f'_CUDA version: ')
+print('*'*10)
+print(f'CUDNN version: {torch.backends.cudnn.version()}')
+print(f'Available GPU devices: {torch.cuda.device_count()}')
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'Using {device} device')
+
 model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights='ResNet18_Weights.DEFAULT')
 for param in model.parameters():
     param.requires_grad = False
@@ -14,7 +25,7 @@ for name, param in model.named_parameters():
         param.requires_grad = True
     else:
         param.requires_grad = False
-print(model)
+model.to(device)
 model.train()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
@@ -25,10 +36,12 @@ class SARImageDataset(Dataset):
         self.transform = transform
         self.img_labels = []
         for label in os.listdir(img_dir):
+            print(f"Label: {label}")
             label_dir = os.path.join(img_dir, label)
             if os.path.isdir(label_dir):
                 for img_file in os.listdir(label_dir):
                     self.img_labels.append((os.path.join(label_dir, img_file), int(label)))
+        print(f"Found {len(self.img_labels)} images in {img_dir}")     
 
     def __len__(self):
         return len(self.img_labels)
@@ -45,8 +58,8 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-training_dataset = SARImageDataset("/Users/mitchellsylvia/SAR-Oil-Onboard/Datasets/CSIRO_Sentinel-1_SAR_image_dataset/training_data/", transform=transform)
-dataloader = DataLoader(training_dataset, batch_size=32, shuffle=True)
+training_dataset = SARImageDataset(r"C:\Users\belgi\OneDrive\Documents\GitHub\SAR-Oil-Onboard\Datasets\training_data", transform=transform)
+dataloader = DataLoader(training_dataset, batch_size=256, shuffle=True)
 
 # Training loop
 num_epochs = 10
@@ -54,6 +67,7 @@ for epoch in range(num_epochs):
     running_loss = 0.0
     for images, labels in tqdm(dataloader, desc=f"Epoch {epoch+1}/{num_epochs}"):
         # Zero the parameter gradients
+        images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
 
         # Forward pass
@@ -69,6 +83,7 @@ for epoch in range(num_epochs):
     print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(dataloader)}")
 
 filename = "resnet18_sar.pt"
-torch.save(model.state_dict(), f"/Users/mitchellsylvia/SAR-Oil-Onboard/src/models/ResNet/{filename}")
+base_dir = r"C:\Users\belgi\OneDrive\Documents\GitHub\SAR-Oil-Onboard\src\models\ResNet"
+torch.save(model.state_dict(), f"{base_dir}/{filename}")
 print(f"Finished Training! File saved as {filename}")
     
