556e7d4d549d158e2f050cceba8d370095e2ea53
Mitchell Sylvia <msylvia@umass.edu>
2025-03-27 19:32:15 -0400

    More changes

--- src/dataset_processing/pngPreprocessing.py
+++ src/dataset_processing/pngPreprocessing.py
@@ -19,22 +19,15 @@ class PNGProcessing():
                     resized_alpha = cv2.resize(a, (0, 0), fx=0.1, fy=0.1)  # Resize alpha channel to half its size
                 else:
                     resized_alpha = a  # Use original alpha channel if dimensions are manageable
-                edges = cv2.Canny(resized_alpha, 50, 150)
-                cv2.imshow("edges", edges)
+                color_alpha = cv2.cvtColor(resized_alpha, cv2.COLOR_GRAY2BGR)  # Convert to 3-channel image
+                cv2.imshow("alpha", color_alpha)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
-                if edges is not None and edges.any():
-                    hough_lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=10, minLineLength=50, maxLineGap=0)  # Adjusted parameters for better detection of thin lines
-                else:
-                    hough_lines = None
-                if hough_lines is not None:
-                    color_alpha = cv2.cvtColor(resized_alpha, cv2.COLOR_GRAY2BGR)  # Convert to 3-channel image
-                    for line in hough_lines:
-                        x1, y1, x2, y2 = line[0]
-                        cv2.line(color_alpha, (x1, y1), (x2, y2), (0, 255, 0), 20)
-                    cv2.imshow("Processed PNG", color_alpha)
-                    cv2.waitKey(0)
-                    cv2.destroyAllWindows()
+                
+                
+                cv2.imshow("Processed PNG", color_alpha)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
             return image
         except Exception as e:
             print(f"Error opening PNG file with OpenCV: {e}")
