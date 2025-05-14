c84a7dc1f39ef1eda8136de4f3f033a3799b1f85
Mitchell Sylvia <msylvia@umass.edu>
2025-05-14 18:28:03 -0400

    Try scale factor of 8 with cv2

--- src/datasets/images/baseImage.py
+++ src/datasets/images/baseImage.py
@@ -1,12 +1,9 @@
 from abc import abstractmethod
 import cv2
-from PIL import Image
-import numpy as np
 
 class BaseImage:
     def __init__(self, path: str):
-        image = Image.open(path).resize((image.width // 8, image.height // 8)).convert("LA")
-        self.image = np.array(image)
+        self.image = cv2.imread(path, cv2.IMREAD_REDUCED_COLOR_8)
 
     def width(self):
         return self.image.shape[1]
