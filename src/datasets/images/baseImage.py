56bf2fc6b7ed04566af3b67216766e56ce42a962
Mitchell Sylvia <msylvia@umass.edu>
2025-05-14 18:25:47 -0400

    resize and convert in one go

--- src/datasets/images/baseImage.py
+++ src/datasets/images/baseImage.py
@@ -5,8 +5,7 @@ import numpy as np
 
 class BaseImage:
     def __init__(self, path: str):
-        image = Image.open(path).convert("LA")
-        image = image.resize((image.width // 4, image.height // 4))
+        image = Image.open(path).resize((image.width // 4, image.height // 4)).convert("LA")
         self.image = np.array(image)
 
     def width(self):
